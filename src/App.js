942c64c1dfd037efc11d2a6ea4964c760d9d5e1c
Kyle <kat.udacity@gmail.com>
2019-08-31 08:23:11 +0800

    natural shop

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
+import items from '../components/Cart/';
+import Product from '../components/Product/Product';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-header-text">Dreamcast Shop</h1>
       </header>
+      <main className="App-shop">
+        <div className="App-products">
+          {items.map(item => (
+            <Product key={item.title} title={item.title} price={item.price} />
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
-
-export default App;
---
+++ src/api/api.js
@@ -0,0 +1,49 @@
+export default [
+  {
+    id: 1,
+    title: 'C24/7',
+    price: 40,
+    category: 'Health Supplements',
+  },
+  {
+    id: 2,
+    title: 'Complete',
+    price: 39,
+    category: 'Health Supplements',
+  },
+  {
+    id: 3,
+    title: 'Choleduz',
+    price: 35,
+    category: 'Health Supplements',
+  },
+
+  {
+    id: 4,
+    title: 'Restorlyf',
+    price: 49,
+    category: 'Health Supplements',
+  },
+
+  {
+    id: 5,
+    title: 'Max',
+    price: 49,
+    category: 'Health Supplements',
+  },
+
+  {
+    id: 6,
+    title: 'Drink',
+    price: 35,
+    category: 'Health Supplements',
+  },
+
+  {
+    id: 7,
+    title: 'White',
+    price: 49,
+    category: 'Health Supplements',
+  },
+
+];
